fec0496d657686b6f087f481890886db562f6936
Felipe <felipemarino91@gmail.com>
2019-03-02 02:53:44 -0300

    Initial Commit

---
+++ DynamicTypeDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DynamicTypeDemo
+//
+//  Created by Felipe Marino on 02/03/19.
+//  Copyright © 2019 Felipe Marino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
